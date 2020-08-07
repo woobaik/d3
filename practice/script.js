c25a1e75f39ec27bcc80f67bb9db5023e4b420c4
Jbaik <joungwoo.baik@gmail.com>
2020-08-07 00:33:23 -0700

    FInish linear graph

--- practice/script.js
+++ practice/script.js
@@ -11,6 +11,11 @@ const graph = svg
 	.append("g")
 	.attr("transform", `translate(${margin.left}, ${margin.top})`)
 
+const xAxisGroup = graph
+	.append("g")
+	.attr("transform", `translate(0,${graphHeight})`)
+const yAxisGroup = graph.append("g")
+
 d3.json("data.json").then((data) => {
 	const rects = graph.selectAll("rect").data(data)
 
@@ -25,13 +30,20 @@ d3.json("data.json").then((data) => {
 	const y = d3
 		.scaleLinear()
 		.domain([0, d3.max(data, (data) => data.population)])
-		.range([0, 500])
+		.range([graphHeight, 0])
 
 	rects
 		.enter()
 		.append("rect")
 		.attr("width", x.bandwidth)
-		.attr("height", (d) => y(d.population))
+		.attr("height", (d) => graphHeight - y(d.population))
 		.attr("x", (d, i, n) => i * 90)
 		.attr("fill", "pink")
+		.attr("y", (d) => y(d.population))
+
+	const xAxis = d3.axisBottom(x)
+	const yAxis = d3.axisLeft(y)
+
+	xAxisGroup.call(xAxis)
+	yAxisGroup.call(yAxis)
 })

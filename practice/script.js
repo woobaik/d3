5f3540615c1d2d77e58a719a42cdc7445106bd2c
Jbaik <joungwoo.baik@gmail.com>
2020-08-12 21:37:01 -0700

    Finish this graph without any help :)

--- practice/script.js
+++ practice/script.js
@@ -1,49 +1,50 @@
-const svg = d3
-	.select(".canvas")
-	.append("svg")
-	.attr("width", 600)
-	.attr("height", 600)
+const margin = { top: 20, right: 20, left: 50, bottom: 15 }
+const svg = d3.select("svg")
 
-const margin = { top: 20, right: 50, bottom: 20, left: 50 }
-const graphWidth = 600 - margin.left - margin.right
-const graphHeight = 600 - margin.top - margin.bottom
-const graph = svg
-	.append("g")
-	.attr("transform", `translate(${margin.left}, ${margin.top})`)
+const graphHeight = svg.attr("width") - margin.left - margin.right
 
-const xAxisGroup = graph
-	.append("g")
-	.attr("transform", `translate(0,${graphHeight})`)
-const yAxisGroup = graph.append("g")
-
-d3.json("data.json").then((data) => {
+const render = (data) => {
+	const graph = svg
+		.append("g")
+		.attr("transform", `translate(${margin.left}, ${margin.top})`)
 	const rects = graph.selectAll("rect").data(data)
 
-	const min = d3.min(data, (data) => data.population)
-	console.log(min)
-
-	const x = d3
-		.scaleBand()
-		.domain(data.map((d) => d.city))
-		.range([0, 500])
+	const xAxisGroup = graph
+		.append("g")
+		.attr("transform", `translate(0, ${graphHeight})`)
+	const yAxisGroup = graph.append("g")
 
 	const y = d3
 		.scaleLinear()
-		.domain([0, d3.max(data, (data) => data.population)])
+		.domain([0, d3.max(data, (d) => d.population)])
 		.range([graphHeight, 0])
 
-	rects
-		.enter()
-		.append("rect")
-		.attr("width", x.bandwidth)
-		.attr("height", (d) => graphHeight - y(d.population))
-		.attr("x", (d, i, n) => i * 90)
-		.attr("fill", "pink")
-		.attr("y", (d) => y(d.population))
+	const x = d3
+		.scaleBand()
+		.domain(data.map((item) => item.city))
+		.range([0, 900])
+		.padding(0.2)
 
 	const xAxis = d3.axisBottom(x)
 	const yAxis = d3.axisLeft(y)
 
 	xAxisGroup.call(xAxis)
 	yAxisGroup.call(yAxis)
+
+	rects
+		.enter()
+		.append("rect")
+		.attr("width", x.bandwidth())
+		.attr("height", (d) => graphHeight - y(d.population))
+		.attr("x", (d) => x(d.city))
+		.attr("y", (d) => y(d.population))
+		.attr("fill", "orange")
+}
+
+d3.json("data.json").then((data) => {
+	data.forEach((item) => {
+		item.population = Math.round(item.population / 1000)
+	})
+
+	render(data)
 })
